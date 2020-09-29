5be2107fe83a5a5f19814f786230734d3481efe1
Rachel Peterson <rachelp5446@gmail.com>
2020-09-29 10:36:10 -0500

    Update existing files

--- functionsIntermediate1.py
+++ functionsIntermediate1.py
@@ -1,1 +1,11 @@
+import random
+def randInt(min=0, max=100):
+    num = random.random() * max
+    return round(num)
 
+print(randInt())
+print(randInt(max=100)) 
+print(randInt(max=50))
+print(randInt(min=50, max=100))
+print(randInt(min=50))
+print(randInt(min=50, max=500))
